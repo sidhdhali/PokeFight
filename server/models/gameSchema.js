2991159aca7f041539f71c8ed7fe1db6b71024a8
sidhdhali <sidhnakrani1911@gmail.com>
2024-05-31 15:56:49 +0200

    part4 remian

---
+++ server/models/gameSchema.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const gameSchema = new mongoose.Schema({
+  playerPokemon: {
+    type: String,
+    required: true
+  },
+  opponentPokemon: {
+    type: String,
+    required: true
+  },
+  result: {
+    type: String, 
+    required: true
+  },
+  turns: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default:Date.now
+  }
+})
+
+
+export default mongoose.model('Game', gameSchema);
